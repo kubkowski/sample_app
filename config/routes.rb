30be3b36730461cbd4467e2ccd7f8457a260ae1c
kubkowski <jakub.opara@gmail.com>
2014-04-15 15:52:46 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   
+  resources :users
   get "users/new"
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
